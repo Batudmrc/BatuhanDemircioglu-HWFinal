5630b1813f988186ef0bb52e73a88aaba6dd5dc7
Batuhan Demircioğlu <bthn_chn_dmrc@hotmail.com>
2023-06-15 18:00:23 +0300

    code refactored

---
+++ SearchTunes/Extensions/HomePage+UISetup.swift
@@ -0,0 +1,78 @@
+//
+//  HomePage+UISetup.swift
+//  SearchTunes
+//
+//  Created by Batuhan Demircioğlu on 15.06.2023.
+//
+
+import Foundation
+import UIKit
+
+extension HomeViewController {
+    
+    func setupUI() {
+        setupSearchBar()
+        setGradient()
+        setupNavController()
+    }
+    
+    func setupNavController() {
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.tintColor = UIColor.green
+        UINavigationBar.appearance().tintColor = UIColor.green
+    }
+    
+    func setupEmptyView() {
+        messageLabel.textAlignment = .center
+        messageLabel.textColor = .white
+        messageLabel.font = UIFont.systemFont(ofSize: 20)
+        messageLabel.text = "No items to display"
+        tableView.addSubview(messageLabel)
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.centerXAnchor.constraint(equalTo: tableView.centerXAnchor).isActive = true
+        messageLabel.centerYAnchor.constraint(equalTo: tableView.centerYAnchor).isActive = true
+    }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: HomePageTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: HomePageTableViewCell.identifier)
+        tableView.showsVerticalScrollIndicator = false
+    }
+    
+    func setupSearchBar() {
+        searchBar.delegate = self
+        searchBar.placeholder = "Search for a track"
+        searchBar.backgroundImage = UIImage()
+        searchBar.searchBarStyle = .minimal
+        searchBar.tintColor = .black
+        if let searchTextField = searchBar.value(forKey: "searchField") as? UITextField {
+            if let leftView = searchTextField.leftView {
+                searchTextField.leftViewMode = .always
+                searchTextField.autocorrectionType = .no
+                let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: leftView.frame.height)) // Adjust padding width here
+                searchTextField.leftView = paddingView
+                searchTextField.backgroundColor = .white
+                paddingView.addSubview(leftView)
+            }
+        }
+        searchBar.layer.shadowColor = UIColor.black.cgColor
+        searchBar.layer.shadowOffset = CGSize(width: 0, height: 2)
+        searchBar.layer.shadowRadius = 4
+        searchBar.layer.shadowOpacity = 0.3
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.textColor = .black
+            textField.font = UIFont.systemFont(ofSize: 16)
+            textField.backgroundColor = .white
+            textField.layer.cornerRadius = 8
+            textField.layer.masksToBounds = true
+            textField.autocorrectionType = .no
+        }
+    }
+    
+    
+    
+    
+}
--- SearchTunes/Scenes/HomePage/HomeViewController.swift
+++ SearchTunes/Scenes/HomePage/HomeViewController.swift
@@ -49,11 +49,6 @@ class HomeViewController: UIViewController {
         topView.layer.insertSublayer(gradientLayer, at: 0)
     }
     
-    func setupUI() {
-        setupSearchBar()
-        setGradient()
-    }
-    
     @IBAction func favoriteButtonTapped(_ sender: Any) {
         presenter.favoriteButtonTapped()
     }
@@ -93,60 +88,13 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension HomeViewController: HomeViewControllerProtocol, UISearchBarDelegate {
     
-    func setupEmptyView() {
-        messageLabel.textAlignment = .center
-        messageLabel.textColor = .white
-        messageLabel.font = UIFont.systemFont(ofSize: 20)
-        messageLabel.text = "No items to display"
-        tableView.addSubview(messageLabel)
-        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        messageLabel.centerXAnchor.constraint(equalTo: tableView.centerXAnchor).isActive = true
-        messageLabel.centerYAnchor.constraint(equalTo: tableView.centerYAnchor).isActive = true
-    }
+    
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         presenter.searchBarTextDidChange(searchText)
     }
     
-    func setupTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(UINib(nibName: HomePageTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: HomePageTableViewCell.identifier)
-        tableView.showsVerticalScrollIndicator = false
-    }
     
-    func setupSearchBar() {
-        searchBar.delegate = self
-        searchBar.placeholder = "Search for a track"
-        searchBar.backgroundImage = UIImage() // Remove background image
-        searchBar.searchBarStyle = .minimal // Apply minimal style
-        searchBar.tintColor = .black // Set tint color for cursor and cancel button
-        // Add padding to the left side of the search bar's leftBarButtonItem
-        if let searchTextField = searchBar.value(forKey: "searchField") as? UITextField {
-            if let leftView = searchTextField.leftView {
-                searchTextField.leftViewMode = .always
-                searchTextField.autocorrectionType = .no
-                let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: leftView.frame.height)) // Adjust padding width here
-                searchTextField.leftView = paddingView
-                searchTextField.backgroundColor = .white
-                paddingView.addSubview(leftView)
-            }
-        }
-        searchBar.layer.shadowColor = UIColor.black.cgColor
-        searchBar.layer.shadowOffset = CGSize(width: 0, height: 2)
-        searchBar.layer.shadowRadius = 4
-        searchBar.layer.shadowOpacity = 0.3
-        // Customize search bar text field
-        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
-            textField.textColor = .black // Set text color
-            textField.font = UIFont.systemFont(ofSize: 16) // Set font
-            textField.backgroundColor = .white
-            textField.layer.cornerRadius = 8 // Set corner radius
-            textField.layer.masksToBounds = true
-            textField.autocorrectionType = .no
-            // Clip to bounds
-        }
-    }
     
     func reloadData() {
         DispatchQueue.main.async { [weak self] in
